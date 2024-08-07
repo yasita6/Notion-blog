18707912a617ffe2050ebcb24bc2cce0350a914c
CloCo <dreamworldplay6@gmail.com>
2024-08-07 23:40:14 +0900

    記事を取得

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,27 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import Head from "next/head";
+import { getAllPosts } from "../lib/notionAPI";
 
-const inter = Inter({ subsets: ["latin"] });
+export const getStaticProps = async () => {
+  const allPosts = await getAllPosts() || [];
 
-export default function Home() {
+  return {
+    props:{
+      allPosts,
+    },
+    revalidate: 60,
+  };
+
+};
+
+export default function Home({allPosts}: any) {
+  console.log(allPosts);
   return (
     <div>
+      <Head>
+        <title>Notion Blog</title>
+        <meta name="description" content="Notionを使ってブログを作成します" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
     </div>
   );
 }
